bc27dd3ac1ea361565a39e9e9e2c7fa8c187398b
r2omm-kieko <67681263+r2omm-kieko@users.noreply.github.com>
2023-02-09 13:21:40 +0630

    add cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ActivatedRoute } from '@angular/router';
-import { Product, products } from '../products';
 
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +13,7 @@ export class ProductDetailsComponent implements OnInit{
 
   product : Product | undefined;
 
-  constructor( private route :ActivatedRoute) {}
+  constructor( private route :ActivatedRoute, private cartService : CartService) {}
 
   ngOnInit(){
 
@@ -22,5 +22,13 @@ export class ProductDetailsComponent implements OnInit{
 
     this.product = products.find( product => product.id === productIdFormRoute);
   }
+
+  addToCart(product :Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+
+  }
+
+  
 
 }
